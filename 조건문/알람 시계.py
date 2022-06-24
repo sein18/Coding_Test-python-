b5bf519bae134361d6223693049e2e5f207e7e9c
tpdls7080@naver.com <tpdls7080@naver.com>
2022-06-24 22:52:06 +0900

    백준 문제풀기

--- 조건문/알람 시계.py
+++ 조건문/알람 시계.py
@@ -1,12 +1,13 @@
 text = input()
+ 
 a,b,c = map(int,text.split()) 
-x=[a,b,c]
-x.sort(reverse=True)
-if x[0]==x[1]==x[2]:
-    print(x[0]*1000+10000)
-elif x[0]==x[1]:
-    print(1000+x[0]*100)
-elif x[1]==x[2]:
-    print(1000+x[1]*100)
+
+if b+x>=60:
+    a+=(b + x)//60
+    if a>=24:
+        a-=24
+    b=(b + x) % 60
+    print(a,b)
 else:
-    print(x[0]*100)
+    print(a,b + x)
+
