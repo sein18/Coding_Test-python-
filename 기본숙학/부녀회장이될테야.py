7390028e3a4a14321a18239378b34e070ed1f8bc
tpdls7080@naver.com <tpdls7080@naver.com>
2022-06-27 19:37:13 +0900

    코딩테스트 준비

--- 기본숙학/부녀회장이될테야.py
+++ 기본숙학/부녀회장이될테야.py
@@ -1,10 +1,16 @@
-"""x=[()]
-for i in range(14):
-    x[0][i]=1
+re = int(input())
+for i in range(re):
+    k = int(input())
+    n = int(input())
+    s = 0
+    
+    apart = [[0]*(n + 1) for t in range(k + 1)] 
 
-print(x[0])"""
-b=None;
-a={'a':97,'b':98,'c':99}
-print(a,b)
-if b==None:
-    print(1)
+    for q in range(n+1):
+            apart[0][q] = q
+  
+    for i in range(k):
+        for j in range(n):
+            apart[i+1][j+1]=apart[i][j+1]+apart[i+1][j]
+    print(apart[k][n])
+
