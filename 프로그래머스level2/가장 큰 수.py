4466bd05e7c9d0bf6d5732bcbbef4b237b530a1d
tpdls7080@naver.com <tpdls7080@naver.com>
2022-07-03 21:45:27 +0900

    코테준비

---
+++ 프로그래머스level2/가장 큰 수.py
@@ -0,0 +1,6 @@
+
+t = [3, 30, 34, 5, 9]
+temp= []
+for i in range(len(t)):
+    temp.append(str(t[i])*3)
+print(temp)
---
+++ 프로그래머스level2/다리를 지나는 트럭.py
@@ -0,0 +1,19 @@
+from tokenize import Number
+
+
+car = [7,4,5,6]
+car1 =[]
+t=0
+bridge_length=2
+truck_weight=10
+num=0
+bsize=0
+for i in range(10000):
+
+    if(len(car1)<=bridge_length):
+        break
+
+    if(car==[]):
+        break
+
+print(sum(car))
