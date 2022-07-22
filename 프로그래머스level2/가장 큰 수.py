868b5b0f1b488aa456927326876dae14cd4ec3ae
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2022-07-22 19:07:41 +0900

    프로그래머스 코딩테스트

--- 프로그래머스level2/가장 큰 수.py
+++ 프로그래머스level2/가장 큰 수.py
@@ -1,6 +1,11 @@
 
+
+
 t = [3, 30, 34, 5, 9]
-temp= []
-for i in range(len(t)):
-    temp.append(str(t[i])*3)
-print(temp)
+ 
+
+def solution(numbers):
+    numbers = sorted(list(map(str,numbers)),key=lambda x: x*3,reverse=True)
+    return str(int(''.join(numbers)))
+
+print(solution(t))
--- 프로그래머스level2/짝지어제거하기.py
+++ 프로그래머스level2/짝지어제거하기.py
@@ -1,16 +1,12 @@
 def solution(s):
     answer = -1
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
  
     x=[]
     for i in s:
         x.append(i)
         if len(x) >= 2 and x[len(x)-2] == x[len(x)-1]:
             x.pop()
-            x.pop() 
-        print(x)
+            x.pop()  
     if x==[]:
         answer=1
     else:
