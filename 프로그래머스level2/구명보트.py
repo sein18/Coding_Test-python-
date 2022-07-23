82aaf9ff071aee254ca0584b088db4f38377f989
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2022-07-23 21:31:33 +0900

    프로그래머스 코딩테스트 연습

---
+++ 프로그래머스level2/구명보트.py
@@ -0,0 +1,21 @@
+def solution(people, limit):
+    answer = 0
+    
+    people.sort()
+   
+    sum=0
+    num=0 
+    idx=0
+    while len(people)!=1:
+        sum += people[idx]
+        if sum > limit:
+            answer+=1
+            for i in range(idx):
+                print(i)
+                people.pop(0)
+            sum=0
+            idx=-1
+        idx+=1
+    return answer+1
+
+print(solution([70, 80, 50],100))
