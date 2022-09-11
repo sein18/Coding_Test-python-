7fabd3f9f49913a661477413e63d31f27d1df3d3
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2022-09-11 16:51:12 +0900

    백준 알고리즘

---
+++ 그리디알고리즘/동전0.py
@@ -0,0 +1,13 @@
+N, change = map(int, input().split())
+coin_list = []
+for n in range(N):
+    coin_list.append(int(input()))
+    
+count = 0
+for coin in coin_list[::-1]:
+    if coin == 0: break
+    
+    count += change // coin
+    change %= coin
+    
+print(count) 
