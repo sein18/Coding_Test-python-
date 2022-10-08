6a4c0c2ece46a60246be107a7b1c895d62398108
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2022-10-08 10:42:20 +0900

    쉬운 계단 수

---
+++ 동적 계획법1/쉬운계단수.py
@@ -0,0 +1,14 @@
+n=int(input())
+d=[[0]*10 for _ in range(n+1)]
+mod=10**9
+
+for i in range(1, 10):
+    d[1][i]=1
+
+for i in range(1, n):
+    d[i+1][0]=d[i][1]
+    d[i+1][9]=d[i][8]
+    for j in range(1, 9):
+        d[i+1][j]=d[i][(j+1)%10]+d[i][(j+9)%10]
+
+print(sum(d[-1])%mod) 
