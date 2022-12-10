a56b14c0ce1964e38bed995ddccb58f0a3127f86
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2022-12-10 21:23:44 +0900

    정리

--- 함수모음.py
+++ 함수모음.py
@@ -1,36 +1,50 @@
 # max, min
 x= [9.96,1.27,5.07,8.38]
 print(max(x),min(x))
+
 # 문자열
 txt = 'abcdefghigk'
 print(txt[5:8])
+
 # 문자열 홀수
 print(txt[::2])
+
 # 거꾸로 츨력
 print(txt[8:5:-1])
+
 # 문자열에 특정 문자열 확인
 if 'abc' in txt:
     print("text")
+
 # 문자열 길이 len
 print(len(txt))
+
 # 문자열 알바벳검사 isalpha()
 print(txt.isalpha())
+
 # 문자열 숫자확인 isdigit()
 print(txt.isdigit())
+
 # 문자열이 알파벳또는 숫자인지 검사 isalnum()
 print(txt.isalnum())
+
 # 문자열 대소문자 변환 (upper.lower)
 print(txt.upper(),txt.lower())
+
 # 문자열 좌우 공백제거 (lstrip,rstrip,strip)
 print(txt.lstrip(),txt.rstrip(),txt.strip())
+
 # 문자열에 있는 문자(열)개수 구하기(count)
 print(txt.count('a'))
+
 # 문자열에서 특정 문자(열)위치 찾기(find)
 print(txt.find('igk'))
+
 # 문자열을 특정문자(열)로 분리 (split)
 url = 'a/cd/fe/ddd/wwq/aa/dd'
 rel = url.split('/')
 print(rel)
+
 # 문자열을 특정 문자(열)로 결합하기(join)
 rel1 = ':'
 print(rel1.join(rel))
