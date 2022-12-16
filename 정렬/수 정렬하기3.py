ff3717122269a1c74634adf4ec218eb236c0e39d
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2022-12-16 20:36:44 +0900

    정리

--- 정렬/수 정렬하기3.py
+++ 정렬/수 정렬하기3.py
@@ -1,13 +1,13 @@
 import sys
  
-arr=[0]*10000
+arr=[0] * 10000
 num = int(sys.stdin.readline())
 for i in range(num):
     inum=(int(sys.stdin.readline()))
-    arr[inum-1]+=1
+    arr[inum-1] += 1
 
 for i in range(10000):
-    if arr[i]!=0:
+    if arr[i] != 0:
         for j in range(arr[i]):
             print(i+1)
  
