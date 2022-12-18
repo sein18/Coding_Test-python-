d45c51dc9c90eb2b6e884b1b189546f64caf7243
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2022-12-18 11:37:33 +0900

    정리

--- 스택/괄호.py
+++ 스택/괄호.py
@@ -4,11 +4,11 @@ for i in range(num):
     li=[]
     for j in range(len(str)):
         li.append(str[j])
-        if len(li)>1:
-            if li[len(li)-2]=='(' and li[len(li)-1]==')':
+        if len(li) > 1:
+            if li[len(li)-2] == '(' and li[len(li)-1]==')':
                 del(li[len(li)-2])
                 del(li[len(li)-1]) 
-    if(len(li)==0):
+    if(len(li) == 0):
         print('YES')
     else:
         print('NO')
