a99186bd2142133a5b64e53ad913f811c62d8fae
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2023-01-06 23:34:29 +0900

    정리

--- 정렬/나이순정렬.py
+++ 정렬/나이순정렬.py
@@ -1,7 +1,7 @@
 import sys
 
 N = int(sys.stdin.readline().strip())
-a= [[]for N in range(N)]
+a = [[]for N in range(N)]
 
 for i in range(N):
     age, user_name = sys.stdin.readline().split()
@@ -9,8 +9,8 @@ for i in range(N):
     a[i].append(user_name)
     a[i].append(i)
 
-a.sort(key=lambda registerd_order: registerd_order[2])
-a.sort(key=lambda registerd_order: registerd_order[0])
+a.sort(key = lambda registerd_order: registerd_order[2])
+a.sort(key = lambda registerd_order: registerd_order[0])
 
 for j in a:
     print('{} {}'.format(j[0],j[1])) 
