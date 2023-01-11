e267ac93a1bf3e6f61b2bfd73ce992b3f4b72641
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2023-01-11 20:41:54 +0900

    정리

--- 반복문/더하기사이클.py
+++ 반복문/더하기사이클.py
@@ -1,13 +1,13 @@
-x=int(input())
-y=x
-num=1
+x = int(input())
+y = x
+num = 1
 for i in range(9999):
-    a=y//10 
-    b=y%10 
-    c=a+b 
-    y=(y%10)*10+c%10 
+    a = y // 10 
+    b = y % 10 
+    c = a + b  
+    y = (y % 10) * 10 + c % 10 
     if(y != x):
-        num+=1
+        num += 1
     else:
         break
 print(num)
