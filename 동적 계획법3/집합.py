115eb72881b67735354eff31d336a83a02545c54
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2023-01-15 09:46:51 +0900

    정리

--- 동적 계획법3/집합.py
+++ 동적 계획법3/집합.py
@@ -1,23 +1,23 @@
 import sys
 input=sys.stdin.readline
 
-num=[0]*21
+num = [0]*21
 for _ in range(int(input())):
     line = input().split()
-    cmd=line[0]
-    if cmd=="all":
-        num=[1]*21
-    elif cmd=="empty":
-        num=[0]*21
+    cmd = line[0]
+    if cmd == "all":
+        num = [1]*21
+    elif cmd == "empty":
+        num = [0]*21
     else:
-        n=int(line[1])
-        if cmd=="add": 
-            num[n]=1
-        elif cmd=="remove":
-            num[n]=0
-        elif cmd=="check":
+        n = int(line[1])
+        if cmd == "add": 
+            num[n] = 1
+        elif cmd == "remove":
+            num[n] = 0
+        elif cmd == "check":
             if num[n]: print(1)
             else: print(0)
         else:
-            if num[n]: num[n]=0
-            else: num[n]=1 
+            if num[n]: num[n] = 0
+            else: num[n] = 1 
