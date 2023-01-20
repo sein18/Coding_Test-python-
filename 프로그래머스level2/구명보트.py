30e430606fdb13aa11a5dbf921ca18835b35fda4
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2023-01-20 22:11:08 +0900

    정리

--- 프로그래머스level2/구명보트.py
+++ 프로그래머스level2/구명보트.py
@@ -3,18 +3,18 @@ def solution(people, limit):
     
     people.sort()
    
-    sum=0
-    num=0 
-    idx=0
+    sum = 0
+    num = 0 
+    idx = 0
     while len(people)!=1:
         sum += people[idx]
         if sum > limit:
-            answer+=1
+            answer += 1
             for i in range(idx): 
                 people.pop(0)
-            sum=0
-            idx=-1
-        idx+=1
-    return answer+1
+            sum = 0
+            idx = -1
+        idx += 1
+    return answer + 1
 
 print(solution([70, 80, 50],100))
