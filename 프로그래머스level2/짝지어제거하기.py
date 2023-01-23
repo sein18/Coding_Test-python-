fc905aded1f2288bbc4ee771477274817385f56a
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2023-01-23 16:33:58 +0900

    종리

--- 프로그래머스level2/짝지어제거하기.py
+++ 프로그래머스level2/짝지어제거하기.py
@@ -1,16 +1,16 @@
 def solution(s):
     answer = -1
  
-    x=[]
+    x = []
     for i in s:
         x.append(i)
         if len(x) >= 2 and x[len(x)-2] == x[len(x)-1]:
             x.pop()
             x.pop()  
-    if x==[]:
-        answer=1
+    if x == []:
+        answer = 1
     else:
-        answer=0
+        answer = 0
     return answer
 
 
