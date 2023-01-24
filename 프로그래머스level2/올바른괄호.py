377de5880ed873119d093eb8337ac536e8c809c8
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2023-01-24 21:31:30 +0900

    정리

--- 프로그래머스level2/올바른괄호.py
+++ 프로그래머스level2/올바른괄호.py
@@ -1,12 +1,12 @@
 def solution(s):
     answer = True
-    x=[]
+    x = []
     for i in s:
         x.append(i)
-        if len(x) >= 2 and x[len(x)-2]=='(' and x[len(x)-1]==')':
+        if len(x) >= 2 and x[len(x)-2] == '(' and x[len(x)-1] == ')':
             x.pop()
             x.pop() 
-    if len(x)==0:
+    if len(x) == 0:
         return True 
     else:
         return False
