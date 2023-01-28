194321529abf8d1c4a9b8dba6cff04b3417e52d3
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2023-01-28 16:49:49 +0900

    정리

--- 프로그래머스level2/행렬의곱샘.py
+++ 프로그래머스level2/행렬의곱샘.py
@@ -3,5 +3,5 @@ def solution(arr1, arr2):
     for i in range(len(arr1)):
         for j in range(len(arr2[0])):
             for k in range(len(arr1[0])):
-                answer[i][j]+=(arr1[i][k]*arr2[k][j])
+                answer[i][j] += (arr1[i][k]*arr2[k][j])
     return answer
