957c4b71f548d1a6a81db8dbf1be99a14dd93e43
박세인(Park sein) <62274431+sein18@users.noreply.github.com>
2023-02-01 23:02:09 +0900

    정리

--- 프로그래머스level1/기능개발.py
+++ 프로그래머스level1/기능개발.py
@@ -3,10 +3,10 @@ def solution(progresses, speeds):
     num = 0
     while len(progresses) != 0:
         for i in range(len(progresses)):
-            progresses[i]+=speeds[i]
-        if(progresses[0]>=100):
+            progresses[i] += speeds[i]
+        if(progresses[0] >= 100):
             for i in range(len(progresses)):
-                j=i
+                j = i
                 if(progresses[j-num]>=100):
                     del(progresses[j-num])
                     del(speeds[j-num])
